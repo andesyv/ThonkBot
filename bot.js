ed9ce26bcdffcca8d3ed3c5955b62dc745e803e4
Anders Syvertsen <andesyv@gmail.com>
2018-09-21 19:55:43 +0200

    Added some testcode for a webinterface
    Credits to synicalsyntax for his guide: https://github.com/synicalsyntax/discord.js-heroku

--- bot.js
+++ bot.js
@@ -76,3 +76,32 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 bot.on('disconnect', function (errMsg, code) {
     logger.log('info', 'Disconnected with error message: ' + errMsg);
 });
+
+// Testcode taken from: https://github.com/synicalsyntax/discord.js-heroku/blob/web/index.js
+// Credits to synicalsyntax
+
+// Web app (Express + EJS)
+const http = require('http');
+const express = require('express');
+const app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 5000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the `public` directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', (request, response) => {
+    // ejs render automatically looks in the views folder
+    response.render('index');
+});
+
+app.listen(port, () => {
+    // will echo 'Our app is running on http://localhost:5000 when run locally'
+    console.log('Our app is running on http://localhost:' + port);
+});
