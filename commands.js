53de8966196b316359693f28ad90c056ee63bb4d
Anders Syvertsen <andesyv@gmail.com>
2018-11-16 15:47:41 +0100

    Added a filter
    Big commit (sorry for not dividing it further into smaller commits).
    The filter is special extra rules that you can apply to the secret santa command to further customize the results.
    Also moved all christmas related stuff to a different module placed in /lib.

--- commands.js
+++ commands.js
@@ -1,6 +1,7 @@
 var Discord = require('discord.js');
 var fs = require('fs');
 const jokes = require('./jokes.json');
+const christmasThonk = require('./lib/thonkbot-christmas');
 
 // Converts the message to a command and runs it.
 exports.runCommand = function (bot, message, logger) {
@@ -59,93 +60,17 @@ function parseCommand(bot, cmd, args, message, logger) {
             break;
         case 'PEPPERKAKE':
             if (message.channel instanceof Discord.TextChannel) {
-                sendPepperkake(message, logger);
+                christmasThonk.sendPepperkake(message, logger);
             }
             break;
         case 'SECRETSANTA':
             if (message.channel instanceof Discord.TextChannel) {
-                secretSanta(message, logger);
+                christmasThonk.secretSanta(message, logger);
             }
             break;
         default:
             break;
      }
-}
-
-function secretSanta (message, logger) {
-    let participants = Array.from(message.mentions.users.values());
-
-    // Check for crash
-    if (participants == null || !Array.isArray(participants) || participants.length < 1) {
-        message.channel.send('You have to specify users who is participating in the secret santa by "mentioning" them. ( !secretSanta @firstPerson @secondPerson )');
-        return;
-    } else if (participants.length == 1) {
-        message.channel.send("You can't have a secret santa with yourself. Mention more users.");
-        return;
-    } else if (isAnyUserBots(participants)) {
-        message.channel.send("Bots can't participate in a secret santa. Mention users only.");
-        return;
-    }
-
-    // Copy participants array into receivers and shuffle it.
-    let receivers = participants.slice();
-    receivers = shuffle(receivers);
-
-    // Shuffle as long as there are elements that are the same in the arrays.
-    while (hasSharedElement(participants, receivers)) {
-        receivers = shuffle(receivers);
-    }
-
-    // Send a message to each participant with their corresponding receiver.
-    for (let i = 0; i < participants.length; i++) {
-        let newDMChannel = participants[i].createDM();
-        newDMChannel.then((value) => {
-            value.send("Your secret santa is: " + receivers[i].tag);
-        }).catch(() => {
-            logger.log('error', `Failed to create dm channel with user ${participants[i].tag} on textChannel ${message.channel.name}`);
-        });
-    }
-}
-
-function isAnyUserBots(a) {
-    if (Array.isArray(a)) {
-        for (let i = 0; i < a.length; i++) {
-            if (a[i] instanceof Discord.User && a[i].bot) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-// Checks if for every index i in array there are any a[i] === b[i]
-function hasSharedElement(a, b) {
-    if (Array.isArray(a) && Array.isArray(b)) {
-        for (let i = 0; i < a.length && i < b.length; i++) {
-            if (a[i] === b[i]) {
-                return true;
-            }
-        }
-        return false;
-    } else {
-        return a === b;
-    }
-}
-
-function sendPepperkake (message, logger) {
-    let mentioned = message.mentions.users.first();
-    // Check for crash
-    if (mentioned == null) {
-        message.channel.send('Nei!');
-        return;
-    }
-
-    let newDMChannel = mentioned.createDM();
-    newDMChannel.then((value) => {
-        value.send('Du må gi en pepperkake til ' + message.author.tag);
-    }).catch(() => {
-        logger.log('error', `Failed to create dm channel with user ${mentioned.tag} on textChannel ${message.channel.name}`);
-    });
 }
 
 function sendManySpooks (message) {
