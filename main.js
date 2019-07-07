e9d480b3b9a702a24f687381a733b0e10def7a72
andesyv <andesyv@gmail.com>
2019-07-07 03:28:58 +0200

    Disabled the web application
    The web application part was mostly for pinging the app to keep it awake, but I have now moved the app to another server that (hopefully) won't go to sleep. If it seems necessary, the web application part can be enabled again.

--- main.js
+++ main.js
@@ -42,33 +42,33 @@ bot.login(auth.token);
 // Testcode taken from: https://github.com/synicalsyntax/discord.js-heroku/blob/web/index.js
 // Credits to synicalsyntax
 
-// Web app (Express + EJS)
-const https = require('https');
-const express = require('express');
-const app = express();
-
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-const port = process.env.PORT || 5000;
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the `public` directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
-// set the home page route
-app.get('/', (request, response) => {
-    // ejs render automatically looks in the views folder
-    response.render('index');
-});
-
-app.listen(port, () => {
-    // will echo 'Our app is running on http://localhost:5000 when run locally'
-    console.log('Our app is running on http://localhost:' + port);
-});
-
-// pings server every 15 minutes to prevent dynos from sleeping (cheeky)
-setInterval(() => {
- https.get('https://thonk-bot.herokuapp.com');
-}, 900000);
+// // Web app (Express + EJS)
+// const https = require('https');
+// const express = require('express');
+// const app = express();
+//
+// // set the port of our application
+// // process.env.PORT lets the port be set by Heroku
+// const port = process.env.PORT || 5000;
+//
+// // set the view engine to ejs
+// app.set('view engine', 'ejs');
+//
+// // make express look in the `public` directory for assets (css/js/img)
+// app.use(express.static(__dirname + '/public'));
+//
+// // set the home page route
+// app.get('/', (request, response) => {
+//     // ejs render automatically looks in the views folder
+//     response.render('index');
+// });
+//
+// app.listen(port, () => {
+//     // will echo 'Our app is running on http://localhost:5000 when run locally'
+//     console.log('Our app is running on http://localhost:' + port);
+// });
+//
+// // pings server every 15 minutes to prevent dynos from sleeping (cheeky)
+// setInterval(() => {
+//  https.get('https://thonk-bot.herokuapp.com');
+// }, 900000);
